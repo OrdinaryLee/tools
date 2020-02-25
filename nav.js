618add2032514c688af98461fc1f97850da3d3be
OrdinaryLee <48003099+OrdinaryLee@users.noreply.github.com>
2020-02-26 03:57:07 +0800

    my commit message

--- nav.js
+++ nav.js
@@ -1,1 +1,1 @@
-var nav = [{"name": "tools/nav.js at master \u00b7 OrdinaryLee/tools", "url": "https://github.com/OrdinaryLee/tools/blob/master/nav.js"}]
+var nav = [{"name": "tools/nav.js at master \u00b7 OrdinaryLee/tools", "url": "https://github.com/OrdinaryLee/tools/blob/master/nav.js"}, {"name": "Python 3 Module of the Week \u2014 PyMOTW 3", "url": "https://pymotw.com/3/index.html"}]
